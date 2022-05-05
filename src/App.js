771e8c707e0829590e76da0545590bd6afc4cb0b
siddiq <siddiq.aiub@gmail.com>
2022-05-05 11:54:56 +0600

    design add product form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import AddProduct from './components/Pages/AddProduct/AddProduct';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
    <div>
       <Header></Header>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/addProduct' element={<AddProduct></AddProduct>}></Route>
+    </Routes>
     <Footer></Footer>
     </div>
   );
--- src/components/Pages/AddProduct/AddProduct.js
+++ src/components/Pages/AddProduct/AddProduct.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 const AddProduct = () => {
-    return (
-        <div>
-            add product
+  return (
+    <div className="container mt-5">
+      <Form>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Label>Product Stock Date</Form.Label>
+          <Form.Control type="date" placeholder="" />
+        </Form.Group>
+      <Row>
+      <Form.Group as={Col} className="mb-3">
+          <Form.Label>Product Category</Form.Label>
+          <Form.Select>
+            <option>Select Product Category</option>
+            <option>a</option>
+            <option>b</option>
+            <option>c</option>
+            <option>d</option>
+            <option>e</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Label>Product Name</Form.Label>
+          <Form.Select>
+            <option>Select Product Name</option>
+            <option>a</option>
+            <option>b</option>
+            <option>c</option>
+            <option>d</option>
+            <option>e</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Label>Product Supplier</Form.Label>
+          <Form.Select>
+            <option>Select Product Supplier</option>
+            <option>a</option>
+            <option>b</option>
+            <option>c</option>
+            <option>d</option>
+            <option>e</option>
+          </Form.Select>
+        </Form.Group>
+      </Row>
+        
+        <Row>
+        <Form.Group as={Col} className="mb-3" controlId="">
+          <Form.Label>Product Unit</Form.Label>
+          <Form.Control type="text" placeholder="" />
+        </Form.Group>
+        <Form.Group as={Col} className="mb-3" controlId="">
+          <Form.Label>Product Quantity</Form.Label>
+          <Form.Control type="text" placeholder="" />
+        </Form.Group>
+        <Form.Group as={Col} className="mb-3" controlId="">
+          <Form.Label>Product Price</Form.Label>
+          <Form.Control type="text" placeholder="" />
+        </Form.Group>
+
+        </Row>
+        <div className="d-grid">
+        <Button variant="primary" size='lg' type="submit">
+          Add Product
+        </Button>
         </div>
-    );
+       
+      </Form>
+    </div>
+  );
 };
 
 export default AddProduct;
