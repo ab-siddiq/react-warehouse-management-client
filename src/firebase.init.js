2cb62625948178100167844fa9797e8b1b85a431
siddiq <siddiq.aiub@gmail.com>
2022-05-05 11:19:10 +0600

    set up react project and install necessary utilities

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEqE99025lp-9PyFVFnWVJEjBDZu_xEyI",
+  authDomain: "react-node-diagnostic-equip.firebaseapp.com",
+  projectId: "react-node-diagnostic-equip",
+  storageBucket: "react-node-diagnostic-equip.appspot.com",
+  messagingSenderId: "200835729532",
+  appId: "1:200835729532:web:f4c29c732f7b47d24721f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
